7cd281798705128900fcf0847e5945752f311926
Nivian96 <vika.nik.1996@mail.ru>
2022-04-07 22:55:55 +0300

    header validation

--- lesson3_ex12.py
+++ lesson3_ex12.py
@@ -1,13 +1,16 @@
 import requests
 
 class TestHeader:
+
+    expected_header_key = "x-secret-homework-header"
+    expected_header_value = "Some secret value"
+
     def test_check_header(self):
         url = "https://playground.learnqa.ru/api/homework_header"
         response = requests.get(url)
         headers = response.headers
 
-        expected_header_key = "x-secret-homework-header"
-        expected_header_value = "Some secret value"
-
-        assert headers.get(expected_header_key) is not None, f"There is no {expected_header_key} in {list(headers.keys())}"
-        assert headers.get(expected_header_key) == expected_header_value, f"Value of {expected_header_key} is not equal to {expected_header_value}"
+        assert headers.get(self.expected_header_key) is not None, \
+            f"There is no {self.expected_header_key} in {list(headers.keys())}"
+        assert headers.get(self.expected_header_key) == self.expected_header_value, \
+            f"Value of {self.expected_header_key} is not equal to {self.expected_header_value}"
